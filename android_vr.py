a3c71914bbc137327e21dc42e64991d8412199fd
dcarrillo11 <carrillomartind@gmail.com>
2024-07-02 14:22:38 +0200

    Android automation completed

--- android_vr.py
+++ android_vr.py
@@ -1,18 +1,49 @@
 from ppadb.client import Client
+import time
+from tkinter import messagebox
 
-# Default is "127.0.0.1" and 5037
-adb = Client(host="127.0.0.1", port=5037)
-devices = adb.devices()
 
-if len(devices) == 0:
-    print('no device attached')
-    quit()
+def android_connect():
 
-device = devices[0]
+    adb = Client(host="127.0.0.1", port=5037)
+    devices = adb.devices()
 
-screen_state = device.shell(f'dumpsys power | grep mScreenOn ')
-print(screen_state)
+    if len(devices) == 0:
+        messagebox.showerror(title="Conection error", message = "Móvil desconectado o innaccesible")
+    else:
+        device_adb = devices[0]
+        return device_adb
 
-device.shell(f'input keyevent 26')
-device.shell(f'input touchscreen swipe 930 2500 1080 380')
-device.shell(f'input text "9420"')
+def start_vr(device):
+    # Default is "127.0.0.1" and 5037
+    adb = Client(host="127.0.0.1", port=5037)
+    devices = adb.devices()
+
+    if len(devices) == 0:
+        return False
+
+    device = devices[0]
+
+    device.push(f'./Media/merged.mp4 sdcard/Alphamini/')
+
+    screen_state = device.shell(f'dumpsys display | grep mScreenState=')
+
+    if "OFF" in screen_state:
+        device.shell(f'input keyevent 26')
+        device.shell(f'input swipe 930 2500 1080 380')
+        device.shell(f'input text "9420"')
+
+    device.shell(f'am start -n com.xojot.vrplayer/.MainActivity')
+    time.sleep(3)
+    device.shell(f'input tap 530 2880')
+    device.shell(f'input tap 530 2880')
+    device.shell(f'input tap 400 950')
+    device.shell(f'input tap 1350 1550')
+    device.shell(f'input tap 1350 100')
+    device.shell(f'input tap 1300 720')
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
