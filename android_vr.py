26d17fc4c3a07990fc8526720f6d6f4c4bd66b86
dcarrillo11 <carrillomartind@gmail.com>
2024-07-08 10:42:36 +0200

    Added functionality for server and screen status

--- android_vr.py
+++ android_vr.py
@@ -1,9 +1,24 @@
-from ppadb.client import Client
+
 import time
+from subprocess import Popen,PIPE
+from ppadb.client import Client
 from tkinter import messagebox
 
+def start_server():
+
+    Popen(['adb.exe', 'start-server'], stderr=PIPE, stdout=PIPE)
+    time.sleep(2)
+    Popen(['scrcpy'], stderr=PIPE, stdout=PIPE)
+    print("daemon server online")
+
+def kill_server():
+    
+    Popen(['adb.exe', 'kill-server'], stderr=PIPE, stdout=PIPE)
+    print("daemon server offline")
 
 def android_connect():
+
+    start_server()
 
     adb = Client(host="127.0.0.1", port=5037)
     devices = adb.devices()
@@ -20,14 +35,18 @@ def start_vr(device):
     device.push("./media/merged.mp4", "/sdcard/Alphamini/merged.mp4")
 
     screen_state = device.shell(f'dumpsys display | grep mScreenState=')
+    screen_lock = device.shell(f'dumpsys window | grep -oE "mDreamingLockscreen=....."')
 
     if "OFF" in screen_state:
         device.shell(f'input keyevent 26')
         device.shell(f'input swipe 930 2500 1080 380')
         device.shell(f'input text "9420"')
+    elif "true" in screen_lock:
+        device.shell(f'input swipe 930 2500 1080 380')
+        device.shell(f'input text "9420"')
+    else:
+        device.shell(f'input keyevent 3')
 
-    #device.shell(f'input keyevent 3')
-    #device.shell(f'input keyevent 3')
     device.shell(f'pm clear com.xojot.vrplayer')
     device.shell(f'media volume --set 0')
     device.shell(f'am start -n com.xojot.vrplayer/.MainActivity')
@@ -41,15 +60,16 @@ def start_vr(device):
     device.shell(f'input tap 210 2680')
     device.shell(f'input tap 1350 100')
     device.shell(f'input tap 1300 720')
-    time.sleep(2.5)
+    time.sleep(2)
     device.shell(f'media volume --set 7')
     device.shell(f'input tap 1500 720')
 
 def stop_vr(device):
     device.shell(f'pm clear com.xojot.vrplayer')
+    kill_server()
 
 def main():
-    device_adb = android_connect()
+    vr_flag, device_adb = android_connect()
     start_vr(device_adb)
 
 if __name__ == '__main__':
