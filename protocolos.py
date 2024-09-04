745ed5fff6d274bc1e4ce7e5fc11d36fd4ec1a2a
dcarrillo11 <carrillomartind@gmail.com>
2024-09-04 09:28:10 +0200

    Moved record protocols to a different file

---
+++ protocolos.py
@@ -0,0 +1,103 @@
+import os
+import sys
+import time
+import asyncio
+import random
+from datetime import date, datetime
+
+import psutil
+import pandas as pd
+import matplotlib.pyplot as plt 
+import tkinter as tk
+import tkinter.ttk as ttk
+from tkinter import messagebox
+#import cv2 as cv
+import pygame
+from moviepy.editor import VideoFileClip
+from pydub import AudioSegment
+from pydub.playback import play
+from pylsl import StreamInlet, resolve_stream
+
+import mini.mini_sdk as MiniSdk
+from mini.apis import *
+from mini.dns.dns_browser import WiFiDevice
+
+from menu import play_video_3, play_audio, record_data
+from test_connect import test_connect, test_get_device_by_name, test_play_action
+from android_vr import android_connect, start_vr, stop_vr
+from movie_editor import vr_maker
+
+def relax_protocol(inlet, protocol_type, relax_time = 10, start = True):
+
+    if start:
+        if protocol_type == 'control':
+            play_video_3('./Media/Comienzo.mp4')
+        elif protocol_type == ('video' or 'robot'):
+            play_video_3('./Media/Comienzo_detalle.mp4')
+        else:
+            pass
+    else:
+        if protocol_type == 'control':
+            play_video_3('./Media/Relax.mp4')
+        elif protocol_type == ('video' or 'robot'):
+            play_audio('./Media/Relax.mp3')
+        else:
+            pass
+
+    print('Inicio relax '+datetime.now().strftime('%Y%m%d%H%M')+'\n')
+    data_dict = record_data(relax_time, inlet)
+    df_relax = pd.DataFrame.from_dict(data_dict)
+    df_relax['STI'] = 4 #Relax label for the data
+    print('Fin relax '+datetime.now().strftime('%Y%m%d%H%M')+'\n')
+
+    return df_relax
+
+
+def control_protocol(mov):
+    
+    if mov == 'right':
+        play_video_3('./Media/Imagina_dch.mp4')
+    elif mov == 'left':
+        play_video_3('./Media/Imagina_izq.mp4')
+    else:
+        play_video_3('./Media/Imagina_ambos.mp4')
+
+    
+def robot_protocol(mov):
+
+    if mov == 'right':
+        asyncio.get_event_loop().run_until_complete(test_play_action('face_028b'))
+    elif mov == 'left':
+        asyncio.get_event_loop().run_until_complete(test_play_action('Surveillance_004'))
+    else:
+        asyncio.get_event_loop().run_until_complete(test_play_action('random_short5'))
+
+
+def video_protocol(mov):
+
+    if mov == 'right':
+        play_video_3('./Media/alphamini_right_front.mp4')
+    elif mov == 'left':
+        play_video_3('./Media/alphamini_left_front.mp4')
+    else:
+        play_video_3('./Media/alphamini_both_front.mp4')  
+
+
+def vr_protocol(movements_list, device = None):
+    
+    if type(movements_list) is list:
+        #vr_maker(movements_list)
+        start_vr(device)
+        time.sleep(23)
+    else:
+        pass
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+
+
+
+
